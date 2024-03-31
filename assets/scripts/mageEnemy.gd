bc295326668ec3ff25a37bee34417d880a8ccfc9
uwie <louis.binwag@student.ateneo.edu>
2024-04-01 04:19:26 +0800

    Update mageEnemy.gd

--- assets/scripts/mageEnemy.gd
+++ assets/scripts/mageEnemy.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 @onready var warrior_enemy = $"../WarriorEnemy"
+@onready var notification = $"../PlayerNotif"
 
 var health = 100
 var turn_speed = 70
@@ -31,14 +32,18 @@ func attack(enemy):
 	else:
 		enemy.health -= enemy.damage_taken(damage)
 	
+	if enemy.damage_taken(damage) == 0:
+		notification.text = "Enemy Dodged!"
 	# TODO: if damage taken = 0, output "dodged!"
-		
+
 func damage_taken(damage):
 	var mitigated_damage = damage * (1 - defense / 100)
 
 	if truth_chance(dodge_chance):
 		mitigated_damage = 0  # Dodge occurred, so damage is completely mitigated
+		notification.text = "Player Dodged!"
 	elif truth_chance(crit_chance):
 		mitigated_damage *= 2  # Critical hit occurred, so double the damage
+		notification.text = "Critical!"
 
 	return mitigated_damage
