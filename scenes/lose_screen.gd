e4760f7c937207f05c2a8f5d3285d4c9f7f738f2
Paolo <118610859+Pencisas@users.noreply.github.com>
2024-03-31 23:36:38 +0800

    Added Lose Screen
    - added lose screen functionalities when player loses

---
+++ scenes/lose_screen.gd
@@ -0,0 +1,18 @@
+class_name losescreen
+extends Control
+
+@onready var play_again_button = $MarginContainer/HBoxContainer/VBoxContainer/Play_Again as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+
+@onready var start_level = preload("res://scenes/world.tscn")
+
+func _ready():
+	play_again_button.button_down.connect(on_play_again_pressed)
+	exit_button.button_down.connect(on_quit_pressed)
+	
+func on_play_again_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+	
+func on_quit_pressed() -> void:
+	get_tree().quit()
+
